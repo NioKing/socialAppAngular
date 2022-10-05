4c4b60ef7c53eddb3ea6fc863d6489dec8ade65d
Nio <nio@example.com>
2022-10-05 13:42:20 +0300

    added confirm password validator

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { gql } from 'apollo-angular';
 import { AuthService } from '../services/auth.service';
+import { CustomValidators } from './validators/match.validator';
 
 @Component({
   selector: 'app-register',
@@ -19,10 +20,12 @@ export class RegisterComponent implements OnInit {
   registerForm = new FormGroup({
     email: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(30)]),
-    // confirmPassword: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
     country: new FormControl(null),
     dateOfBirth: new FormControl(null)
-  })
+  },
+  [CustomValidators.MatchValidator('password', 'confirmPassword')]
+  )
 
   ngOnInit(): void {
   }
@@ -30,8 +33,8 @@ export class RegisterComponent implements OnInit {
 
   onSignUp() {
     let { email, password, country, dateOfBirth } = this.registerForm.value
-    this.authService.signUp(email!, password!, country!, dateOfBirth!).subscribe((val: any) => {
-      let user = val.data?.createUser
+    this.authService.signUp(email!, password!, country!, dateOfBirth!).subscribe(async (val: any) => {
+      let user = await val.data?.createUser
       if(user) {
         this.router.navigate(['login'])
       }
