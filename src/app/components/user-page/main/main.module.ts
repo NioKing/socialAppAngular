d6f9748e76e612899f6e764da26b17cf6be71484
Nio <nio@example.com>
2022-11-01 16:49:10 +0300

    added sibebar

--- src/app/components/user-page/main/main.module.ts
+++ src/app/components/user-page/main/main.module.ts
@@ -5,11 +5,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MainComponent } from './main.component';
 import { MainPageRoutingModule } from './main-routing.module';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { SideBarComponent } from '../side-bar/side-bar.component';
 
 
 
 @NgModule({
-    declarations: [MainComponent, NavbarComponent],
+    declarations: [MainComponent, NavbarComponent, SideBarComponent],
     imports: [
         CommonModule,
         SharedModule,
